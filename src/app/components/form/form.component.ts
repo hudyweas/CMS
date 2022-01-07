7de7fa7714d495251cc3638a46fae739886fd20e
hudyweas <73641290+hudyweas@users.noreply.github.com>
2022-01-07 13:27:38 +0100

    changes back

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'cms-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
+
   @Input() formGroup: FormGroup;
   @Input() inputAtrr: [];
+
+  public formArray: FormArray;
+
+  ngOnInit(): void {
+    this.formArray = this.formBuilder.array([this.formGroup]);
+    console.log(this.formArray);
+  }
 }
